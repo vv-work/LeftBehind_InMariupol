c5f3a2354112b0dd0e34bc0b9b16a9c6e5ba423b
Julius <julius.paramono@gmail.com>
2025-08-23 18:47:04 -0700

    last changes

--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -10,7 +10,15 @@ namespace MonoBehaviours
 {
     public class UnitSelectionManager : MonoBehaviour
     {
+        public event EventHandler OnSelectionAreaStart; 
+        public event EventHandler OnSelectionAreaEnd; 
+        public static UnitSelectionManager Instance { get; private set; }
+          
         private Vector2 _selectionStartPosition;
+        private void Awake()
+        {
+            Instance = this;
+        }
 
         private void Update()
         {
@@ -18,12 +26,14 @@ namespace MonoBehaviours
             if (Mouse.current.leftButton.wasPressedThisFrame)
             {
                 _selectionStartPosition = Mouse.current.position.ReadValue();
+                Rect selectionAreaRect = GetSelectionAreaRect();
+                OnSelectionAreaStart?.Invoke(this, EventArgs.Empty);
                 
             }
             if (Mouse.current.leftButton.wasReleasedThisFrame)
-            {
-                
+            { 
                 var selectionEndPosition = Mouse.current.position.ReadValue();
+                OnSelectionAreaEnd?.Invoke(this, EventArgs.Empty);
             }
                 
             if (Mouse.current.rightButton.wasPressedThisFrame)
@@ -44,6 +54,21 @@ namespace MonoBehaviours
                  }
                  entityQuery.CopyFromComponentDataArray(unitMoverArray);
             }
+        }
+
+        private Rect GetSelectionAreaRect()
+        {
+            var mousePos = Mouse.current.position.ReadValue();
+            var lowerLeftCorner = new Vector2(
+                math.min(_selectionStartPosition.x,mousePos.x), 
+                math.min(_selectionStartPosition.y, mousePos.y)
+                );
+            var upperRightCorner = new Vector2(
+                math.max(_selectionStartPosition.x,mousePos.x), 
+                math.max(_selectionStartPosition.y, mousePos.y)
+            );
+            return new Rect(lowerLeftCorner, upperRightCorner - lowerLeftCorner);
+            
         }
     }
 }
--- Assets/Scripts/UI/UnitSelectionManagerUI.cs
+++ Assets/Scripts/UI/UnitSelectionManagerUI.cs
@@ -1,9 +1,28 @@
+using System;
+using MonoBehaviours;
 using UnityEngine;
 
 namespace UI
 {
     public class UnitSelectionManagerUI : MonoBehaviour
-    {
-        
+    { 
+        [SerializeField] private RectTransform _selectionAreaRectTransform;
+
+        private void Start()
+        {
+            UnitSelectionManager.Instance.OnSelectionAreaStart += SelectionStarted;
+            UnitSelectionManager.Instance.OnSelectionAreaEnd += SelectionEnded;
+            _selectionAreaRectTransform.gameObject.SetActive(false);
+        }
+
+        private void SelectionStarted(object sender, EventArgs e)
+        {
+            _selectionAreaRectTransform.gameObject.SetActive(true);
+        }
+        private void SelectionEnded(object sender, EventArgs e)
+        {
+            _selectionAreaRectTransform.gameObject.SetActive(false);
+        }
+
     }
 }
